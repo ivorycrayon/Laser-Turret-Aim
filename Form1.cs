31ed31be8aa271b6c77d163909f6796dfb123904
IvoryCrayon <Ivorycrayon@live.com>
2021-05-01 06:20:29 -0700

    Added voice commands, but kinda broke everything???????

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Media;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 
 namespace Laser_Turret_Aim
 {
@@ -18,23 +20,28 @@ namespace Laser_Turret_Aim
         public Stopwatch servoWatch { get; private set; }
         public Stopwatch idleVoiceLinesWatch { get; private set; }
 
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer voice = new SpeechSynthesizer();       
+
         int soundIndex = 0;
         int fireIndex = 0;
         bool loopdone = true;
         bool laserSwitch = true;
+        string lastPos;
+        int sensitivity = 10;
 
-        System.Media.SoundPlayer hello = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\hellofriend.wav");
-        System.Media.SoundPlayer iSeeYou = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\iseeyou.wav");
-        System.Media.SoundPlayer stillThere = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\stillthere.wav");
-        System.Media.SoundPlayer comeOver = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\comehere.wav");
-        System.Media.SoundPlayer deploy = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_deploy.wav");
-        System.Media.SoundPlayer disabled = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_turret_disabled_4.wav");
-        System.Media.SoundPlayer retire = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_turret_retire_1.wav");
-        System.Media.SoundPlayer retract = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_retract.wav");
-        System.Media.SoundPlayer anyoneThere = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_turret_autosearch_5.wav");
-        System.Media.SoundPlayer fire1 = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_turret_fire_4x_01.wav");
-        System.Media.SoundPlayer fire2 = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_turret_fire_4x_02.wav");
-        System.Media.SoundPlayer fire3 = new System.Media.SoundPlayer(@"C:\Users\Ivory.DESKTOP-J6TK9H0\Google Drive\programming\C\Arduino\Laser Turret Aim\portal\Turret_turret_fire_4x_03.wav");
+        System.Media.SoundPlayer hello = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.hellofriend);
+        System.Media.SoundPlayer iSeeYou = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.iseeyou);
+        System.Media.SoundPlayer stillThere = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.stillthere);
+        System.Media.SoundPlayer comeOver = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.comehere);
+        System.Media.SoundPlayer deploy = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_deploy);
+        System.Media.SoundPlayer disabled = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_turret_disabled_4);
+        System.Media.SoundPlayer retire = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_turret_retire_1);
+        System.Media.SoundPlayer retract = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_retract);
+        System.Media.SoundPlayer anyoneThere = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_turret_autosearch_5);
+        System.Media.SoundPlayer fire1 = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_turret_fire_4x_01);
+        System.Media.SoundPlayer fire2 = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_turret_fire_4x_02);
+        System.Media.SoundPlayer fire3 = new System.Media.SoundPlayer(Laser_Turret_Aim.Properties.Resources.Turret_turret_fire_4x_03);
 
         List<SoundPlayer> idleSounds = new List<SoundPlayer>();
         List<SoundPlayer> fireSounds = new List<SoundPlayer>();
@@ -54,24 +61,137 @@ namespace Laser_Turret_Aim
             idleVoiceLinesWatch = Stopwatch.StartNew();
 
             port.Open();
+
+            _recognizer.SetInputToDefaultAudioDevice();
+
+            _recognizer.LoadGrammarAsync(
+                new Grammar(
+                    new GrammarBuilder(
+                        new Choices(new string[]{
+                            "System on", "System off", "Move up", "Move down", "Move left", "Move right", "Sensitivity up", "Sensisitvity down", "Go loco" }))));
+
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            voice.SelectVoice("Microsoft Hazel Desktop");
+        }
+        private async void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {           
+            
+
+            if (e.Result.Confidence > .80)
+            {
+                string speech = e.Result.Text;
+
+                int xcoord = parseDataX(lastPos);
+                int ycoord = parseDataY(lastPos);
+
+                if (xcoord < 0) xcoord = 0;
+                if (xcoord > 180) xcoord = 180;
+                if (ycoord < 0) ycoord = 0;
+                if (ycoord > 180) ycoord = 180;
+
+                idleVoiceLinesWatch = Stopwatch.StartNew();
+
+                switch (speech)
+                {
+                    case "System on":
+                        On_Mode();
+                        break;
+
+                    case "System off":
+                        Off_Mode();
+                        break;
+
+                    case "Move up":
+                        lastPos = String.Format("X{0}Y{1}", xcoord, ycoord + sensitivity);
+                        voice.SpeakAsync("Yes master");
+                        port.Write(lastPos);
+                        break;
+
+                    case "Move down":
+                        lastPos = String.Format("X{0}Y{1}", xcoord, ycoord - sensitivity);
+                        voice.SpeakAsync("Yes master");
+                        port.Write(lastPos);
+                        break;
+
+                    case "Move left":
+                        lastPos = String.Format("X{0}Y{1}", xcoord + sensitivity, ycoord);
+                        voice.SpeakAsync("Yes master");
+                        port.Write(lastPos);
+                        break;
+
+                    case "Move right":
+                        lastPos = String.Format("X{0}Y{1}", xcoord - sensitivity, ycoord);
+                        voice.SpeakAsync("Yes master");
+                        port.Write(lastPos);
+                        break;
+
+                    case "Sensitivity up":
+                        sensitivity += 2;
+                        voice.SpeakAsync($"Sensitivity is {sensitivity}");
+                        break;
+
+                    case "Sensitivity down":
+                        if (sensitivity > 2) sensitivity -= 2;
+                        voice.SpeakAsync($"Sensitivity is {sensitivity}");
+                        break;
+
+                    case "Go loco":
+                        Go_Loco();
+
+                        break;
+                }
+            }
+            else
+            {
+                // ignore
+            }            
         }
 
-        private async void Form1_Shown(object sender, EventArgs e)
-        {
+        private async void On_Mode()
+        {            
             deploy.PlaySync();
-            port.Write("ON");
+            port.Write("ON"); 
             hello.Play();
-            await Task.Delay(30);
+            await Task.Delay(30);            
             port.Write("X90Y90");
+            lastPos = "X90Y90";
         }
 
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
-        {
+        private async void Off_Mode() //wtf how did i break this??
+        {            
             retract.PlaySync();
             disabled.PlaySync();
             port.Write("OFF");
             retire.PlaySync();
             port.Write("X90Y180");
+            lastPos = "X90Y180";
+        }
+
+        private async void Go_Loco()
+        {
+            var rand = new Random();
+
+            for(int i = 0; i < 25; i++)
+            {
+                int xcoord = rand.Next(180);
+                int ycoord = rand.Next(180);
+                lastPos = String.Format("X{0}Y{1}", xcoord, ycoord);
+                port.Write(lastPos);                
+                await Task.Delay(85);
+            }           
+
+        }
+
+        private async void Form1_Shown(object sender, EventArgs e)
+        {
+            On_Mode();
+        }
+
+        private async void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Off_Mode();
         }
 
         private async void Form1_MouseClick(object sender, MouseEventArgs e)
@@ -117,6 +237,12 @@ namespace Laser_Turret_Aim
                     }
 
                     break;
+
+                case MouseButtons.Middle:
+
+                    //flash laser mode maybe
+
+                    break;
             }
         }
 
@@ -129,6 +255,21 @@ namespace Laser_Turret_Aim
             idleVoiceLinesWatch = Stopwatch.StartNew();
         }
 
+        private int parseDataX(String data)
+        {
+            data = data.Remove(data.IndexOf("Y"));
+            data = data.Remove(data.IndexOf("X"), 1);
+
+            return int.Parse(data);
+        }
+
+        private int parseDataY(String data)
+        {
+            data = data.Remove(0, data.IndexOf("Y") + 1);
+
+            return int.Parse(data);
+        }
+
         public void writeToPort(Point coordinates)
         {
             int xcoord = (180 - coordinates.X / (Size.Width / 180));
@@ -137,7 +278,7 @@ namespace Laser_Turret_Aim
             if (servoWatch.ElapsedMilliseconds > 15)
             {
                 servoWatch = Stopwatch.StartNew();
-
+                lastPos = String.Format("X{0}Y{1}", xcoord, ycoord);
                 port.Write(String.Format("X{0}Y{1}", xcoord, ycoord));
             }
         }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,9 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Laser_Turret_Aim.Properties
-{
+namespace Laser_Turret_Aim.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,52 +19,152 @@ namespace Laser_Turret_Aim.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Laser_Turret_Aim.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream comehere {
+            get {
+                return ResourceManager.GetStream("comehere", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream hellofriend {
+            get {
+                return ResourceManager.GetStream("hellofriend", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream iseeyou {
+            get {
+                return ResourceManager.GetStream("iseeyou", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream stillthere {
+            get {
+                return ResourceManager.GetStream("stillthere", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_deploy {
+            get {
+                return ResourceManager.GetStream("Turret_deploy", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_retract {
+            get {
+                return ResourceManager.GetStream("Turret_retract", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_turret_autosearch_5 {
+            get {
+                return ResourceManager.GetStream("Turret_turret_autosearch_5", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_turret_disabled_4 {
+            get {
+                return ResourceManager.GetStream("Turret_turret_disabled_4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_turret_fire_4x_01 {
+            get {
+                return ResourceManager.GetStream("Turret_turret_fire_4x_01", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_turret_fire_4x_02 {
+            get {
+                return ResourceManager.GetStream("Turret_turret_fire_4x_02", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_turret_fire_4x_03 {
+            get {
+                return ResourceManager.GetStream("Turret_turret_fire_4x_03", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Turret_turret_retire_1 {
+            get {
+                return ResourceManager.GetStream("Turret_turret_retire_1", resourceCulture);
             }
         }
     }
